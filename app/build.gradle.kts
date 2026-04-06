9c743289d49ef7bcde003d6b9be7d25b6178d0eb
veronica <veronica.vinkler@gmail.com>
2026-04-06 14:17:51 +0300

    Adding dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,4 +55,6 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+    implementation("com.squareup.retrofit2:retrofit:3.0.0")
+    implementation("com.squareup.retrofit2:converter-gson:3.0.0")
 }
