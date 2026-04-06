a46c99d02e604ca9cc72cef7248165a1c8f497a9
veronica <veronica.vinkler@gmail.com>
2026-04-06 13:33:52 +0300

    Implementing DAO in QuizDao.kt

---
+++ app/src/main/java/com/example/myapplication/QuizDao.kt
@@ -0,0 +1,28 @@
+package com.example.myapplication
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+//implements logic, helps typesafe, room optimize and validate queries
+@Dao
+interface QuizDao {
+    //Küsimused
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertQuestions(questions: List<Question>)
+
+    @Query("SELECT * FROM questions WHERE category = :category")
+    suspend fun getQuestionsByCategory(category: String): List<Question>
+
+     @Query("DELETE FROM questions")
+     suspend fun clearQuestions()
+
+     //Ajalugu
+     @Insert(onConflict = OnConflictStrategy.REPLACE)
+     suspend fun insertGameResult(result: GameResult)
+
+     @Query("SELECT * FROM game_history ORDER BY score DESC")
+     fun getAllGameResults(): Flow<List<GameResult>>
+}
